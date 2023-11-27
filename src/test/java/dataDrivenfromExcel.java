1d9b49347be7b93e12429f389c667b4192c2eec8
Naveen <naveenknk2931@gmail.com>
2023-11-27 22:27:38 +0530

    first commit

---
+++ src/test/java/dataDrivenfromExcel.java
@@ -0,0 +1,69 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class dataDrivenfromExcel {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream fis=new FileInputStream("C:\\Users\\navee\\Documents\\.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		
+		int sheets = workbook.getNumberOfSheets();
+		for(int i =0;i<sheets;i++)
+		{
+			if(workbook.getSheetName(i).equalsIgnoreCase("Sheet1"))
+					{
+			XSSFSheet sheet=workbook.getSheetAt(i);
+			Iterator<Row> rows =sheet.iterator();
+			Row firstrow =rows.next();
+			Iterator<Cell> ce =firstrow.cellIterator();
+			int k=0;
+			int coloumn=0;
+			ce.next();
+			while(ce.hasNext())
+			{
+				Cell value=ce.next();
+				if(value.getStringCellValue().equalsIgnoreCase("Data1"))
+				{ 
+					coloumn=k;
+				}
+				k++;
+			}
+			
+			System.out.println(coloumn);
+			
+
+			while(rows.hasNext()) 
+			{
+
+				Row r =rows.next();
+				if(r.getCell(coloumn).getStringCellValue().equalsIgnoreCase("Purchase"))
+				{
+					
+					Iterator<Cell> cv=r.cellIterator();
+					while(cv.hasNext()) 
+					{
+					System.out.println(cv.next().getStringCellValue());	
+					}
+				}
+				
+				
+				
+				
+			}
+			
+			
+					}
+			
+
+			
+		}
+
+}
+}
